833a87114142c0c44a873dde927225050c0859e9
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-11-06 17:20:55 +0700

    perbaikan Main Activity

--- app/src/main/java/com/example/praktikum6/MainActivity.kt
+++ app/src/main/java/com/example/praktikum6/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum6
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,34 +15,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum6.ui.theme.Praktikum6Theme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Praktikum6Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
-                    DataApp(
-                        modifier = Modifier
-                    )
+                    DataApp(modifier = Modifier)
                 }
             }
         }
     }
 }
 
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum6Theme {
-        Greeting("Android")
-    }
-}
